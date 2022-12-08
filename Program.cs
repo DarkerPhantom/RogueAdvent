5ebeae61a8467cef78058b02cff200993d1b4a70
DarkerPhantom <118705746+DarkerPhantom@users.noreply.github.com>
2022-12-08 19:11:03 +0000

    Creation

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Advent;
+public class Starter{
+    public static async Task Main(){
+        Console.WriteLine("Which day should be run?");
+        int input = int.Parse(await GetInputAsync());
+
+        if(input==0){
+            Console.Clear();
+            await Main();
+        }else{
+            
+        }
+        
+    }
+
+    public static async Task<string> GetInputAsync(){
+        string? input = await Console.In.ReadLineAsync();
+        
+        if(int.TryParse(input, out int date)){
+            return input;
+        }else{
+            return "0";
+        }
+           
+    }
+}
