0300110881d15b837fdba66f39cd5e81b3dc5625
DarkerPhantom <118705746+DarkerPhantom@users.noreply.github.com>
2022-12-09 14:20:58 +0000

    Day 3 started

--- Days/Day3.cs
+++ Days/Day3.cs
@@ -1,7 +1,16 @@
 namespace Advent.Days;
 
 public partial class Days{
-    public void Day3(){
-        
+    public async void Day3(){
+        Console.Clear();
+        Console.WriteLine("Day 2 started!");
+        Console.WriteLine("Getting the file...");
+
+        string[] input = await System.IO.File.ReadAllLinesAsync(@"/workspaces/RogueAdvent/Resources/Day3.txt");
+
+        foreach(string inp in input){
+            string[] compartments = {inp.Substring(0,inp.Length/2), $""};
+            Console.WriteLine($"Compartment 1: {compartments[0]}, Compartment 2: {compartments[1]}");
+        }
     }
 }
