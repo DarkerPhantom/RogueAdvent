de011dafe819f08ad6a9c36c123feca8150c1360
DarkerPhantom <118705746+DarkerPhantom@users.noreply.github.com>
2022-12-12 18:00:36 +0000

    Started Day 4

--- Days/Day4.cs
+++ Days/Day4.cs
@@ -2,14 +2,6 @@ namespace Advent.Days;
 
 public partial class Days{
     public void Day4(){
-        Console.WriteLine((int)'Z');
-        Console.WriteLine((int)'a');
-        Console.WriteLine((int)'b');
-        Console.WriteLine((int)'c');
-        Console.WriteLine((int)'d');
-        Console.WriteLine((int)'e');
-        Console.WriteLine((int)'z');
-        Console.WriteLine((int)'A'); //65-90 A-Z
-        Console.WriteLine((char)96); //97-122 a-z
+        
     }
 }
