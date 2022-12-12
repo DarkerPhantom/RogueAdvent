590ee01ef382abbd50ee2624b6f3fcd837cd815f
DarkerPhantom <118705746+DarkerPhantom@users.noreply.github.com>
2022-12-12 17:42:49 +0000

    Day 3 Part 1 finish

--- Days/Day3.cs
+++ Days/Day3.cs
@@ -6,41 +6,43 @@ public partial class Days{
         Console.WriteLine("Day 3 started!");
         Console.WriteLine("Getting the file...");
 
-        var dict = new Dictionary<char, int>{
-            {'a', 1}, {'b', 2}
-        };
-
         string[] input = await System.IO.File.ReadAllLinesAsync(@"/workspaces/RogueAdvent/Resources/Day3.txt");
         int priorities = 0;
+        bool found = false;
         int COut = 0;
 
         Console.WriteLine(input.GetLength(0));
 
         foreach(string inp in input){
             string[] compartments = {inp.Substring(0,inp.Length/2), inp.Substring(inp.Length/2)};
-            char[][] items = {
-                compartments[0].ToCharArray(),
-                compartments[1].ToCharArray()
-            };
 
-            for(int i = 0; i < items[0].Length; i++){
-                for(int j = 0; j < items[1].Length; j++){
-                    char found = items[0][i] == items[1][j] ? items[0][i] : ' ';
-                    
-                    if(found != ' '){
+            Console.WriteLine(compartments[0] + " " + compartments[1]);
+
+            foreach(char i in compartments[0]){
+
+                foreach(char j in compartments[1]){
+
+                    if(i == j){
+                        Console.WriteLine(i);
+                        found = true;
                         COut++;
-                        priorities += char.IsLower(found) ? (int)found - 96 : (int)found - 38;
-                        //Console.WriteLine(found + " " + char.IsLower(found) + " " + (char.IsLower(found) ? (int)found - 96 : (int)found - 38));
-                        //Console.WriteLine(found + " " + COut);
+                        priorities += char.IsLower(i) ? (int)i - 96 : (int)i - 38;
                         break;
                     }
+
                 }
+                if(found){
+                    found = false;
+                    break;
+                }                
+
             }
             
-
-            
         }
-        //7493 is too low
+
+        Console.WriteLine(COut);
+
+        //7493 is too low, 10599 is too high
         Console.WriteLine(priorities);
     }
 } 
